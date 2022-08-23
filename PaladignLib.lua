3dcee6b6fbf9433eadb1fb6b5a47b683a69bd04c
paladign <111393518+paladign@users.noreply.github.com>
2022-08-23 10:55:33 -0400

    added color control

--- PaladignLib.lua
+++ PaladignLib.lua
@@ -1,8 +1,11 @@
 local Functions = {
     ["Highlight"] = function(args)
-        print(args)
         if args[2] == true then
             local Highlight = Instance.new("Highlight", args[1])
+            Highlight.FillColor = BrickColor.new("Really Blue").Color
+            if args[3] ~= nil then
+                Highlight.FillColor = args[3]
+            end
         elseif args[2] == false then
             for _, obj in pairs(args[1]:GetChildren()) do
                 if obj:IsA("Highlight") then
