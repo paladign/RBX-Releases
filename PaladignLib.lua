cab5a85ea6b76e1cf30e66c7359ab0eee164a3bd
paladign <111393518+paladign@users.noreply.github.com>
2022-08-23 09:53:05 -0400

    added cases and args to highlight function

--- PaladignLib.lua
+++ PaladignLib.lua
@@ -1,6 +1,14 @@
 local Functions = {
-    ["Highlight"] = function()
-        print("Test")
+    ["Highlight"] = function(args)
+        if args[2] then
+            local Highlight = Instance.new("HighLight", args[1])
+        elseif not args[2] then
+            for _, obj in pairs(args[1]:GetChildren()) do
+                if obj:IsA("HighLight") then
+                    obj:Destroy()
+                end
+            end
+        end
     end
 }
 
