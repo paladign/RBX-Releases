1e816eb9f7de802b90cb5d86746c01c76184a100
paladign <111393518+paladign@users.noreply.github.com>
2022-08-23 09:58:18 -0400

    patches

--- PaladignLib.lua
+++ PaladignLib.lua
@@ -1,8 +1,8 @@
 local Functions = {
     ["Highlight"] = function(args)
-        if args[2] then
+        if args[2] == true then
             local Highlight = Instance.new("HighLight", args[1])
-        elseif not args[2] then
+        elseif args[2] == false then
             for _, obj in pairs(args[1]:GetChildren()) do
                 if obj:IsA("HighLight") then
                     obj:Destroy()
