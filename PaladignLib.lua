9cdb2475f5b5e6973983e78bd92cc3fda39255f4
paladign <111393518+paladign@users.noreply.github.com>
2022-08-24 11:09:55 -0400

    added update function

--- PaladignLib.lua
+++ PaladignLib.lua
@@ -1,5 +1,6 @@
+local ESPs = {}
 local Functions = {
-    ["AddHighlight"] = function(obj, creating, color) --args = Instance, Bool, Color3 (optional)
+    ["Highlight"] = function(obj, creating, color) --args = Instance, Bool, Color3 (optional)
         if creating == true then
             if obj:FindFirstChild("Highlight") then return end
             local Highlight = Instance.new("Highlight", obj)
@@ -16,15 +17,21 @@ local Functions = {
             end
         end
     end,
+    ["UpdateESP"] = function(tab)
+        for _, datatable in pairs(ESPs) do
+            local vec = workspace.CurrentCamera:WorldToViewportPoint(datatable[2])
+            local vec2 = Vector2.new(vec.X, vec.Y)
+            datatable[1].Position = vec2
+        end
+    end,
     ["AddESP"] = function(obj)
         local text = Drawing.new("Text")
         text.Text = "Test"
         text.Size = 4
         text.Color = Color3.new(1,0.5,0.75)
         text.Visible = true
-        local vec = workspace.CurrentCamera:WorldToViewportPoint(obj.Position)
-        local vec2 = Vector2.new(vec.X, vec.Y)
-        text.Position = vec2
+        local datatable = {text, obj}
+        table.insert(ESPs, datatable)
     end
 }
 
